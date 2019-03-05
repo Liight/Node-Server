43205a5a6176cd6c4f1813a0ebe78449df4ec287
Liight <Liight@users.noreply.github.com>
2019-03-06 06:05:00 +1300

    Install and initiate expressJS in project

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const http = require('http'); // allows us to create a browser
 
-const routes = require('./routes');
+const express = require('express'); // imports expressJS
 
-console.log(routes.someText);
+const app = express(); // express app (implements express framework)
 
-// callback function to be called on each server request
-const server = http.createServer(routes.handler);
+app.use((req, res, next) => {
+    console.log("In the middleware!");
+    next(); // next is a function that is passed to the use method for the next function to execute with app.use(), chaining middleware
+}); 
 
-// starts a process where nodeJS will keep this running for incoming requests
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log("In another middleware!");
+    // ... no next here because middleware ends
+    res.send('<h1>Hello from express</h1>'); // allows us to send a response, sets a html content-type automatically
+}); 
+
+const server = http.createServer(app); // callback function to be called on each server request
+
+server.listen(3000); // starts a process where nodeJS will keep this running for incoming requests
