12f9959b59718adfb247597d3223b41608db54f2
Liight <Liight@users.noreply.github.com>
2019-12-20 09:47:46 +1300

    Updated Routing and Navigation. Refactored Controller Logic. Updated Views.

---
+++ controllers/admin.js
@@ -0,0 +1,27 @@
+const Product = require('../models/product'); // import the model class Product
+
+exports.getAddProduct = (req, res, next) => {
+    res.render("admin/add-product", {
+        pageTitle: "Add Product",
+        path: "/admin/add-product",
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
+};
+
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product(req.body.title); // Use the class to construct a new product
+    product.save(); // save the product to the model dataArray
+    res.redirect('/'); // redirect to the shop
+};
+
+exports.getProducts = (req,res,next) => {
+    Product.fetchAll(products => {
+        res.render('admin/products', {
+            prods: products,
+            pageTitle: 'Admin Products',
+            path: '/admin/products',
+        });
+    });
+}
--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,15 @@ const express = require('express'); // import expressJS
 
 const router = express.Router(); // set express.Router
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 // admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // admin/product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
